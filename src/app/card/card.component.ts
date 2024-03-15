1e8a87f65828c242ab5189c75fc0d53b1c2bb7d4
danilovalerio <danilo_vs@hotmail.com>
2024-03-15 17:00:22 -0300

    Card e estilizacao

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
+//decorator with props default
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  selector: 'app-card', //name component for html
+  templateUrl: './card.component.html', //path for template component
+  styleUrl: './card.component.scss', //path for styles for component
 })
-export class CardComponent {
-
-}
+export class CardComponent {}
