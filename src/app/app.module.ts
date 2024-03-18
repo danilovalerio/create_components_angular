d55df0b246e78181deb4c304258f9cc09e9f6ab7
danilovalerio <danilo_vs@hotmail.com>
2024-03-18 09:06:58 -0300

    Import do cardsmodule in appmodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CardsModule } from './cards/cards.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CardsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
