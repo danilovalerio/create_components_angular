470c2b6c76dd489231718dc8bc4a54a1c69c15b6
danilovalerio <danilo_vs@hotmail.com>
2024-03-18 10:24:35 -0300

    CardButtonCancel dentro do CardsModule

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,6 +4,7 @@ import { CardComponent } from '../card/card.component';
 import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
 
 @NgModule({
   declarations: [
@@ -11,11 +12,13 @@ import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.co
     CardRoxoComponent,
     CardButtonComponent,
     CardRoxoButtonComponent,
+    CardButtonCancelComponent,
   ],
   imports: [CommonModule],
   exports: [
     CardComponent,
     CardRoxoComponent,
+    CardButtonCancelComponent,
     //CardButtonComponent,
     //CardRoxoButtonComponent,
   ],
