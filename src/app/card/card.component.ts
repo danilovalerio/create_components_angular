c0211f6b4a13c72bc33b6f9d0642c67b757c943f
danilovalerio <danilo_vs@hotmail.com>
2024-03-18 09:24:44 -0300

    SetTimout Teste Interpolação (Bind)

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html', //path for template component
   styleUrl: './card.component.scss', //path for styles for component
 })
-export class CardComponent {}
+export class CardComponent {
+  type = 'Simples';
+  price = 100;
+
+  getFullPrice() {
+    setTimeout(() => {
+      this.type = 'Simples V2';
+    }, 3000);
+    return 'R$ ' + this.price + ',00/Mês';
+  }
+}
