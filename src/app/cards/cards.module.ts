371182cf53c4b4f6eb61224df1d248aeb3f6c11a
danilovalerio <danilo_vs@hotmail.com>
2024-03-18 09:06:02 -0300

    Components cards move to CardsModule

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent,
+  ],
   imports: [CommonModule],
 })
 export class CardsModule {}
