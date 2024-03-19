dc3e5dd3138b336f9e679ee84c5b19bcdb1a5a3a
danilovalerio <danilo_vs@hotmail.com>
2024-03-19 15:49:02 -0300

    Click no escopo do componente

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  //templateUrl: './card-button.component.html',
-  template: ` <div class="card-button">Adquirir</div> `,
+  templateUrl: './card-button.component.html',
+  //template: ` <div class="card-button">Adquirir</div> `,
   styleUrl: './card-button.component.scss',
 })
-export class CardButtonComponent {}
+export class CardButtonComponent {
+  onButtonClick() {
+    console.log('onButtonClick');
+  }
+}
