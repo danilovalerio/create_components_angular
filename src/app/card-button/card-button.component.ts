16b5122007df83aacff7056c5b18605467287ff6
danilovalerio <danilo_vs@hotmail.com>
2024-03-19 15:52:37 -0300

    @Output - disparo por parte do componente filho

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './card-button.component.scss',
 })
 export class CardButtonComponent {
+  @Output('btnAlias') btnClickEmitter = new EventEmitter<boolean>();
+
   onButtonClick() {
     console.log('onButtonClick');
+
+    //dispara o conteúdo para o comp pai
+    //e podemos tipar o obj enviado para o pai
+    this.btnClickEmitter.emit(true);
   }
 }
