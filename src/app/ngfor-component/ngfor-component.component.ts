1daeb22cb970e7a6333c15d3850f5a962083ef3c
danilovalerio <danilo_vs@hotmail.com>
2024-03-20 15:56:57 -0300

    ngFor - pegando primeiro ultimo e selecionando um item

--- src/app/ngfor-component/ngfor-component.component.ts
+++ src/app/ngfor-component/ngfor-component.component.ts
@@ -6,10 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './ngfor-component.component.scss',
 })
 export class NgforComponentComponent {
+  personSelectedIndex: number | undefined;
+
   listPeoples = [
     { name: 'Maria', age: 90 },
     { name: 'Maria 2', age: 91 },
     { name: 'Maria 3', age: 92 },
     { name: 'Maria 4', age: 93 },
   ];
+
+  selectPerson(index: number) {
+    console.log(index);
+    this.personSelectedIndex = index;
+  }
 }
