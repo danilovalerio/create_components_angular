320b499c77265e9a527edd63b8a24b85816d08ec
danilovalerio <danilo_vs@hotmail.com>
2024-03-21 09:31:02 -0300

    NG STYLE - torna mais dinamico alterar os estilos do que usar o properties binding

--- src/app/ng-style/ng-style.component.ts
+++ src/app/ng-style/ng-style.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   templateUrl: './ng-style.component.html',
-  styleUrl: './ng-style.component.scss'
+  styleUrl: './ng-style.component.scss',
 })
 export class NgStyleComponent {
+  fontSize: number = 30;
+  textColor: 'blue' | 'orange' = 'blue';
+  btnText: 'Azul' | 'Laranja' = 'Azul';
 
+  increaseFontSize() {
+    this.fontSize += 5;
+  }
+
+  decreaseFontSize() {
+    this.fontSize -= 5;
+  }
+
+  toggleFontColor() {
+    if (this.textColor === 'blue') {
+      this.textColor = 'orange';
+      this.btnText = 'Azul';
+    } else {
+      this.textColor = 'blue';
+      this.btnText = 'Laranja';
+    }
+  }
 }
