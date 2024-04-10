4df650777e35efb9ea78450f1b89a932e33d5cfa
danilovalerio <danilo_vs@hotmail.com>
2024-04-10 17:04:22 -0300

    Lista de botões

--- src/app/fundamentos-ii/view-children/view-children.component.ts
+++ src/app/fundamentos-ii/view-children/view-children.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-view-children',
   templateUrl: './view-children.component.html',
-  styleUrl: './view-children.component.scss'
+  styleUrl: './view-children.component.scss',
 })
 export class ViewChildrenComponent {
-
+  buttonList = ['Botão 1', 'Botão 2', 'Botão 3'];
 }
