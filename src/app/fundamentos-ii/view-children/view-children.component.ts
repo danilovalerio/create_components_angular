73463cc42380dfcb8d242a62ffcca4e3414dd0fb
danilovalerio <danilo_vs@hotmail.com>
2024-04-10 17:09:07 -0300

    ViewChildren - console log retornando os itens valorizados

--- src/app/fundamentos-ii/view-children/view-children.component.ts
+++ src/app/fundamentos-ii/view-children/view-children.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-view-children',
   templateUrl: './view-children.component.html',
   styleUrl: './view-children.component.scss',
 })
-export class ViewChildrenComponent {
+export class ViewChildrenComponent implements AfterViewInit {
   buttonList = ['Botão 1', 'Botão 2', 'Botão 3'];
+
+  @ViewChildren('meuBotao')
+  botoesEl!: QueryList<ElementRef<HTMLButtonElement>>;
+
+  ngAfterViewInit() {
+    console.log(this.botoesEl);
+  }
 }
