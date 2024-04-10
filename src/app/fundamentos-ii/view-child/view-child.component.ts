3ec973e732113f7b923f5130cd997de7310e2682
danilovalerio <danilo_vs@hotmail.com>
2024-04-10 15:33:02 -0300

    ViewChild de um componente filho com template variable

--- src/app/fundamentos-ii/view-child/view-child.component.ts
+++ src/app/fundamentos-ii/view-child/view-child.component.ts
@@ -1,3 +1,4 @@
+import { FilhoViewchildComponent } from './../filho-viewchild/filho-viewchild.component';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
@@ -12,6 +13,13 @@ export class ViewChildComponent {
   @ViewChild('minhaDiv')
   minhaDivEL!: ElementRef<HTMLDivElement>;
 
+  /**
+   * FilhoViewchildComponent é o nome da classe do nosso componente
+   * a sua tipagem, sem ElementRef de HTML, pois ele não é.
+   */
+  @ViewChild('filhoComp')
+  filhoCompReferencia!: FilhoViewchildComponent;
+
   updateInputText() {
     this.meuInputEL.nativeElement.value = 'Texto Atualizado';
     console.log(this.meuInputEL);
@@ -23,5 +31,11 @@ export class ViewChildComponent {
 
   updateDiv() {
     this.minhaDivEL.nativeElement.innerText = 'Texto Atualizado na Div!';
+  }
+
+  ola() {
+    //aqui temos acesso aos métodos e váriaveis do componente filho
+    this.filhoCompReferencia.dizerOi();
+    this.filhoCompReferencia.message = 'Eu disse Oiiii!';
   }
 }
