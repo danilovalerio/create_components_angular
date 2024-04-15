5a5bfc1ddb0aa6ec8ba94721c104d337e7ada432
danilovalerio <danilo_vs@hotmail.com>
2024-04-15 17:33:51 -0300

    ElementRef + ViewChild e no Componente por injeção

--- src/app/fundamentos-iii/element-ref/element-ref.component.ts
+++ src/app/fundamentos-iii/element-ref/element-ref.component.ts
@@ -1,12 +1,41 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { style } from '@angular/animations';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-element-ref',
   templateUrl: './element-ref.component.html',
   styleUrl: './element-ref.component.scss',
 })
-export class ElementRefComponent implements AfterViewInit {
+export class ElementRefComponent implements OnInit, AfterViewInit {
   @ViewChild('minhaDiv') divElemento!: ElementRef<HTMLDivElement>;
+
+  /**
+   * Readonly + private para garantir a imutabilidade após ser valorizada
+   * @param _elementoRef
+   */
+  constructor(private readonly _elementoRef: ElementRef) {}
+
+  ngOnInit() {
+    console.log('ElementRef' + this._elementoRef);
+    const divElemento = this._elementoRef.nativeElement.querySelector(
+      '#minha-outra-div'
+    ) as HTMLDivElement;
+
+    divElemento.textContent = 'Sou a outra div';
+    divElemento.style.backgroundColor = 'blue';
+
+    divElemento.addEventListener('click', () => {
+      console.log('Cliquei na div hahah!');
+    });
+
+    console.log('minha outra div referencia:' + divElemento);
+  }
 
   ngAfterViewInit(): void {
     this.divElemento.nativeElement.style.backgroundColor = 'red';
