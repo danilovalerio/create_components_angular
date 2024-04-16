2318fe06c4c31ecdaf1c8a8d46014ab0ce8a7d0b
danilovalerio <danilo_vs@hotmail.com>
2024-04-16 09:14:18 -0300

    Criando a DIV através do INPUT

--- src/app/fundamentos-iii/xss-exemplo/xss-exemplo.component.ts
+++ src/app/fundamentos-iii/xss-exemplo/xss-exemplo.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-xss-exemplo',
   templateUrl: './xss-exemplo.component.html',
-  styleUrl: './xss-exemplo.component.scss'
+  styleUrl: './xss-exemplo.component.scss',
 })
 export class XssExemploComponent {
+  constructor(private _elRef: ElementRef) {}
 
+  criarElemento(inputText: string) {
+    console.log(inputText);
+    const divElement = document.createElement('div');
+
+    divElement.innerHTML = inputText;
+
+    this._elRef.nativeElement.appendChild(divElement);
+  }
 }
