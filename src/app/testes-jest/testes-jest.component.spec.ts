89d1ee629c6c4974902f6591b54a109722a0a561
danilovalerio <danilo_vs@hotmail.com>
2024-04-16 11:40:57 -0300

    Mock e config do arquivo de teste

--- src/app/testes-jest/testes-jest.component.spec.ts
+++ src/app/testes-jest/testes-jest.component.spec.ts
@@ -1,6 +1,18 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestesJestComponent } from './testes-jest.component';
+import { EstadosService } from './services/estados.service';
+import { Observable } from 'rxjs';
+
+// Dados que serao retornados pela nossa classe de mock (MockEstadosService)
+let mockEstadosServiceData: Observable<any[]> | null = null;
+
+class MockEstadosService {
+  listar(): Observable<any[]> | null {
+    return mockEstadosServiceData;
+  }
+}
 
 describe('TestesJestComponent', () => {
   let component: TestesJestComponent;
@@ -8,10 +20,16 @@ describe('TestesJestComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TestesJestComponent]
-    })
-    .compileComponents();
-    
+      imports: [RouterTestingModule],
+      declarations: [TestesJestComponent],
+      providers: [
+        // IMPORTANTE
+        // Aqui dizemos ao Angular que toda vez que EstadosService
+        // for solicitada, será entregue a classe MockEstadosService
+        { provide: EstadosService, useClass: MockEstadosService },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TestesJestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
