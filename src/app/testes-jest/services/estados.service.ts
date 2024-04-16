1176941ec89b088d8c814f5274f0a53ca3bc4358
danilovalerio <danilo_vs@hotmail.com>
2024-04-16 11:11:33 -0300

    Construcao do Service

--- src/app/testes-jest/services/estados.service.ts
+++ src/app/testes-jest/services/estados.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EstadosService {
-  constructor() {}
+  //exemplos de https://consolelog.com.br/teste-unitario-angular-jest-em-services-que-efetuam-requisicoes-http/
+  constructor(private httpClient: HttpClient) {}
+
+  listar(): Observable<any[]> {
+    return this.httpClient.get<any[]>(url);
+  }
 }
