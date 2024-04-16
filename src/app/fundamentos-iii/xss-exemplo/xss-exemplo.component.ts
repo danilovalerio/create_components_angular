db0ea683286aee0149afe638882601486a3848fb
danilovalerio <danilo_vs@hotmail.com>
2024-04-16 09:28:45 -0300

    Render2

--- src/app/fundamentos-iii/xss-exemplo/xss-exemplo.component.ts
+++ src/app/fundamentos-iii/xss-exemplo/xss-exemplo.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-xss-exemplo',
@@ -6,11 +6,23 @@ import { Component, ElementRef } from '@angular/core';
   styleUrl: './xss-exemplo.component.scss',
 })
 export class XssExemploComponent {
-  constructor(private _elRef: ElementRef) {}
+  constructor(
+    private readonly _elRef: ElementRef,
+    private readonly _renderer2: Renderer2
+  ) {}
 
   criarElemento(inputText: string) {
     console.log(inputText);
     const divElement = document.createElement('div');
+
+    divElement.innerHTML = inputText;
+
+    this._elRef.nativeElement.appendChild(divElement);
+  }
+
+  criarElementoRender2(inputText: string) {
+    console.log(inputText);
+    const divElement = this._renderer2.createElement('div');
 
     divElement.innerHTML = inputText;
 
